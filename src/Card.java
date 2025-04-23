712929f680572ab8b1b8004f73216c38ba316984
GabuGalactico <gabrielphruby@gmail.com>
2025-04-23 12:23:23 -0300

    Finished Flashcards Project

---
+++ src/Card.java
@@ -0,0 +1,31 @@
+public class Card {
+    public String question;
+    public String answer;
+
+    public Card(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Question: " + question + "\nAnswer: " + answer;
+    }
+    
+}
